8accaf33c95277e60c79c536d4321569d7353820
Suryansh Jaiswal <suryanshnanu@gmail.com>
2021-12-24 22:11:37 +0530

    add title

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,19 +1,24 @@
 module.exports = {
-    plugins: [
-      {
-        resolve: "@wkocjan/gatsby-theme-intro",
-        options: {
-          theme: {
-            'back': '#edf0f3',
-            'front': '#2b3137',
-            'lead': '#2781ed',
-            'lead-text': '#ffffff',
-            'line': '#ced9e3',
-            'skill-1': '#405060',
-            'skill-2': '#fc8732',
-            'skill-3': '#2ebc50',
-          },
+  siteMetadata: {
+    description: "Personal page of Suryansh Jaiswal",
+    locale: "en",
+    title: "Suryansh Jaiswal",
+  },
+  plugins: [
+    {
+      resolve: "@wkocjan/gatsby-theme-intro",
+      options: {
+        theme: {
+          'back': '#edf0f3',
+          'front': '#2b3137',
+          'lead': '#2781ed',
+          'lead-text': '#ffffff',
+          'line': '#ced9e3',
+          'skill-1': '#405060',
+          'skill-2': '#fc8732',
+          'skill-3': '#2ebc50',
         },
       },
-    ],
-  }
+    },
+  ],
+}
