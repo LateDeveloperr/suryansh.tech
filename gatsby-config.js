b823679502368d68f753fd685bcf50c5dbae597d
Suryansh Jaiswal <suryanshnanu@gmail.com>
2021-12-27 10:46:15 +0530

    color change

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
           'lead': '#2781ed',
           'lead-text': '#ffffff',
           'line': '#ced9e3',
-          'skill-1': '#405060',
+          'skill-1': '#1AA2D4',
           'skill-2': '#fc8732',
           'skill-3': '#2ebc50',
         },
